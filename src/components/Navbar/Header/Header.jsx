752668c5f78fd0106288567cb4841c7190225551
rashidsarkar <rashidrock558@gmail.com>
2023-12-30 18:46:01 +0600

    Add social media icons to the header.

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -1,4 +1,11 @@
-import { FaFacebookF, FaLocationDot } from "react-icons/fa6";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaLinkedinIn,
+  FaLocationDot,
+  FaTwitter,
+} from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 function Header() {
@@ -26,9 +33,18 @@ function Header() {
             <p>+61383766284</p>
           </div>
         </div>
-        <div className="right">
+        <div className="flex gap-2 right">
           <div className="h-[50px] w-[50px] rounded-full bg-white relative">
             <FaFacebookF className="absolute text-3xl text-[#FF5017] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+          </div>
+          <div className="h-[50px] w-[50px] rounded-full bg-white relative">
+            <FaInstagram className="absolute text-3xl text-[#FF5017] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+          </div>
+          <div className="h-[50px] w-[50px] rounded-full bg-white relative">
+            <FaTwitter className="absolute text-3xl text-[#FF5017] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+          </div>
+          <div className="h-[50px] w-[50px] rounded-full bg-white relative">
+            <FaLinkedinIn className="absolute text-3xl text-[#FF5017] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
           </div>
         </div>
       </div>
